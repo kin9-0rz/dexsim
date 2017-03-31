a4162f5341cfdd46dea891b80736dc2ba9cf1584
mikusjelly <mikusjelly@gmail.com>
2017-03-31 14:53:27 +0800

    update

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# dexsim
+#dexsim
 
 A python3 veresion for [dex-oracle](https://github.com/CalebFenton/dex-oracle).
-
+You can read more details on [dex-oracle](https://github.com/CalebFenton/dex-oracle).
 
 ### Install
 
 1. smali
 2. adb
-3. pip install requirements.txt
+3. pip install -r requirements.txt
 
 Note: Please make sure you cmd can run baksmali/smali, adb, java.
 
@@ -16,6 +16,13 @@ Note: Please make sure you cmd can run baksmali/smali, adb, java.
 1. Conect to a Device or Emulator
 2. `dexsim.bat smali_dir/dex/apk`
 
+# Support
 
-
-You can read more details on  [dex-oracle](https://github.com/CalebFenton/dex-oracle).
+- [x] Ljava/lang/String;->\<init>([B)V
+- [x] func(Ljava/lang/String;)Ljava/lang/String;
+- [x] func(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+- [x] func(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+- [ ] func(I)Ljava/lang/String;
+- [ ] func(II)Ljava/lang/String;
+- [x] func(III)Ljava/lang/String;
+- [ ] fun(Ljava/lang/String;)[B ---> Ljava/lang/String;-><init>([B)V
--- libs/dexsim/plugins/string.py
+++ libs/dexsim/plugins/string.py
@@ -14,7 +14,7 @@ __all__ = ["STRING"]
 class STRING(Plugin):
 
     name = "STRING"
-    version = '0.0.2'
+    version = '0.0.3'
     # make_changes = False
 
     def __init__(self, driver, methods, smali_files):
@@ -22,9 +22,11 @@ class STRING(Plugin):
 
     def run(self):
         print('run Plugin: %s' % self.name, end=' -> ')
-        self.__process_one_argument()
+        self.__process_3_argument()
+        self.__process_2_argument()
+        self.__process_1_argument()
 
-    def __process_one_argument(self):
+    def __process_1_argument(self):
 
         '''
 
@@ -43,6 +45,7 @@ class STRING(Plugin):
         INVOKE_STATIC = 'invoke-static \{[vp]\d+}, L([^;]+);->([^\(]+\(Ljava/lang/String;\))Ljava/lang/String;\s+'
 
         p = re.compile('\s+' + self.CONST_STRING + '\s+' + INVOKE_STATIC + self.MOVE_RESULT_OBJECT)
+        print('\s+' + self.CONST_STRING + '\s+' + INVOKE_STATIC + self.MOVE_RESULT_OBJECT)
 
         json_list = []
         target_contexts = {}
@@ -69,6 +72,147 @@ class STRING(Plugin):
 
                 # get method name
                 args_index = line.index('(Ljava/lang/String;)')
+                methodname = line[end + 3:args_index]
+
+                test = {'className': classname, 'methodName': methodname, 'arguments': args, }
+
+                # [{'className':'', 'methodName':'', 'arguments':'', 'id':''}]
+                id = hashlib.sha256(JSONEncoder().encode(test).encode('utf-8')).hexdigest()
+                test['id'] = id
+
+                # get return variable name
+                p3 = re.compile(self.MOVE_RESULT_OBJECT)
+                mro_statement = p3.search(line).group()
+                return_variable_name = mro_statement[mro_statement.rindex(' ') + 1:]
+
+                if id not in target_contexts.keys():
+                    target_contexts[id] = [(mtd, line, '\n\n    const-string %s, ' % return_variable_name)]
+                else:
+                    target_contexts[id].append((mtd, line, '\n\n    const-string %s, ' % return_variable_name))
+
+                # print(test)
+                if test not in json_list:
+                    json_list.append(test)
+
+        self.optimizations(json_list, target_contexts)
+
+    def __process_2_argument(self):
+        ESCAPE_STRING = '''"(.*?)"'''
+        CONST_STRING = 'const-string [vp]\d+, ' + ESCAPE_STRING + '.*\s+'
+        INVOKE_STATIC = 'invoke-static[/\s\w]+\{[vp,\d\s\.]+},\s+L([^;]+);->([^\(]+\(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;\))Ljava/lang/String;\s+'
+
+        p = re.compile('\s+' + CONST_STRING + CONST_STRING + INVOKE_STATIC + self.MOVE_RESULT_OBJECT)
+
+        json_list = []
+        target_contexts = {}
+        for mtd in self.methods:
+            for i in p.finditer(mtd.body):
+                test = {}
+                line = i.group()
+
+                # get arguments
+                args = []
+                match_string = re.compile(CONST_STRING)
+                for j in match_string.finditer(line):
+                    const_str = re.findall("\w+", j.group())[-1]
+                    arg = []
+                    for item in const_str.encode("UTF-8"):
+                        arg.append(item)
+                    args.append("java.lang.String:" + str(arg))
+
+                # get classname
+                start = line.index('}, L')
+                end = line.index(';->')
+                classname = line[start + 4:end].replace('/', '.')
+
+                # get method name
+                args_index = line.index('(Ljava/lang/String;Ljava/lang/String;)')
+                methodname = line[end + 3:args_index]
+
+                test = {'className': classname, 'methodName': methodname, 'arguments': args, }
+
+                # [{'className':'', 'methodName':'', 'arguments':'', 'id':''}]
+                id = hashlib.sha256(JSONEncoder().encode(test).encode('utf-8')).hexdigest()
+                test['id'] = id
+
+                # get return variable name
+                p3 = re.compile(self.MOVE_RESULT_OBJECT)
+                mro_statement = p3.search(line).group()
+                return_variable_name = mro_statement[mro_statement.rindex(' ') + 1:]
+
+                if id not in target_contexts.keys():
+                    target_contexts[id] = [(mtd, line, '\n\n    const-string %s, ' % return_variable_name)]
+                else:
+                    target_contexts[id].append((mtd, line, '\n\n    const-string %s, ' % return_variable_name))
+
+                # print(test)
+                if test not in json_list:
+                    json_list.append(test)
+
+        self.optimizations(json_list, target_contexts)
+
+    def __process_3_argument(self):
+        '''
+            const-string v0, "4db8c06f8baa2fa97518e2fbc78aed7a"
+
+            const-string v1, "dcc6ee73b95b7008865a1241a3f9f2d4"
+
+            const-string v2, "89cf037654ae21bd"
+
+            invoke-static {v0, v1, v2}, Lnpnojrqk/niwjucst/oifhebjg/uihmjzfs/agntdkrh/xumvnbpc/jqwutfvs/dfkxcwot/hcsplder;->alxrefmv(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+
+            move-result-object v0
+
+            ==>
+
+            const-string v0, "------decode result-------"
+
+            ---------------------------------------------------------
+
+            const-string v29, "b3547fe0b848a8c73e30d89c473cd167"
+
+            const-string v30, "07e942bf3a4751a9c188cf652d6dbe03"
+
+            const-string v31, "b1a840dfc932e576"
+
+            invoke-static/range {v29 .. v31}, Lnpnojrqk/niwjucst/oifhebjg/uihmjzfs/agntdkrh/xumvnbpc/jqwutfvs/dfkxcwot/hcsplder;->alxrefmv(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;
+
+            move-result-object v29
+
+        '''
+        ESCAPE_STRING = '''"(.*?)"'''
+        CONST_STRING = 'const-string [vp]\d+, ' + ESCAPE_STRING + '.*\s+'
+        INVOKE_STATIC = 'invoke-static[/\s\w]+\{[vp,\d\s\.]+},\s+L([^;]+);->([^\(]+\(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;\))Ljava/lang/String;\s+'
+
+        p = re.compile('\s+' + CONST_STRING + CONST_STRING + CONST_STRING + INVOKE_STATIC + self.MOVE_RESULT_OBJECT)
+
+        print("this is strings 3")
+        print('\s+' + CONST_STRING + '\s+' + CONST_STRING + '\s+' + CONST_STRING + '\s+' + INVOKE_STATIC + self.MOVE_RESULT_OBJECT)
+        json_list = []
+        target_contexts = {}
+        for mtd in self.methods:
+            for i in p.finditer(mtd.body):
+                test = {}
+                line = i.group()
+
+                # TODO 后续再考虑使用通用的方式获取参数
+                # get arguments
+                args = []
+                match_string = re.compile(CONST_STRING)
+                for j in match_string.finditer(line):
+                    const_str = re.findall("\w+", j.group())[-1]
+                    arg = []
+                    for item in const_str.encode("UTF-8"):
+                        arg.append(item)
+                    args.append("java.lang.String:" + str(arg))
+
+                # get classname
+                start = line.index('}, L')
+                end = line.index(';->')
+                classname = line[start + 4:end].replace('/', '.')
+
+                # get method name
+                args_index = line.index('(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)')
                 methodname = line[end + 3:args_index]
 
                 test = {'className': classname, 'methodName': methodname, 'arguments': args, }
